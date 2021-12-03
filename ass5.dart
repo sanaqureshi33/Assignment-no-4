fc5898e2cb241b6ef56a1a8788e44630bd639d8d
sanaqureshi33 <94166252+sanaqureshi33@users.noreply.github.com>
2021-12-03 21:22:59 +0500

    Add files via upload

---
+++ ass5.dart
@@ -0,0 +1,58 @@
+// import 'dart:html';
+
+void main(){
+//QUESTION NO 1
+// List l1 =["apple","banana","mango"];
+// print(l1);
+
+
+// QUESTION NO 2
+// List l2 =["sana","shehla",true,23.4,12];
+// print(l2);
+
+// //QUESTION NO 3
+// List Qua= ["SSC","HSC","BCS","BS","BCOM","MS","M.PHIL","PHD"];
+// print("1 Index:${Qua[0]}\n 2 Index:${Qua[1]}\n 3 Index:${Qua[2]}\n 4 Index:${Qua[3]}\n 5 Index:${Qua[4]}\n 6 Index:${Qua[5]}\n 7 Index:${Qua[6]}\n 8 Index:${Qua[7]}");
+
+
+//QUESTION NO 4
+// List l3=["Alishba","Areesha","Bareera"];
+// List l4=[480,490,470];
+// print("Student Name:${l3[0]}\t Student Marks:${l4[0]}\t percentage of Student:${l4[0]/500*100}");
+// print("Student Name:${l3[1]}\t Student Marks:${l4[1]}\t percentage of Student:${l4[1]/500*100}");
+// print("Student Name:${l3[2]}\t Student Marks:${l4[2]}\t percentage of Student:${l4[2]/500*100}");
+
+//QUESTION NO 5
+// List Score=[94,99,42,12,103,112,92,14,12];
+// Score.sort();
+// print(Score);
+
+
+//QUESTION NO 6
+
+// List Score=[94,99,42,12,103,112,92,14,12];
+// Score.sort();
+// var b = List.of(Score.reversed);
+// print(b); 
+
+
+
+//QUESTION NO 7
+
+List l6=["apple","mango","banana","Stawberry"];
+if(l6[2]== "") {
+  print("data is present");
+} else{
+ l6.replaceRange(2, 3, ["pear","graphs"]);
+print(l6);
+  
+}
+
+
+
+
+
+
+
+}
+
